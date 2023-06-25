873f052e6749bc0ec84d612451b5e324bd034301
PeterLenon <goshomip@gmail.com>
2023-06-24 22:44:26 -0400

    URL links are updated

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import time
 
 API_KEY = 'f30d6e982b2202e92b1e4d580d4a5a3d096e9d78nrcyurvBbxZgVCwKSM6pld41G'
 number_link_dict = {
-    '7695674373': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&Q_DL=EMD_S8HCpCW5cI6VabX_3Ua4zpnLOCrIrX0_CGC_T0k5r0tr7YabtcY&_g_=g",
-    '8127786648': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&Q_DL=EMD_S8HCpCW5cI6VabX_3Ua4zpnLOCrIrX0_CGC_fB93a7prL242KmD&_g_=g",
-    '7172479172': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&Q_DL=EMD_S8HCpCW5cI6VabX_3Ua4zpnLOCrIrX0_CGC_1nFwlOgSxTcxIxz&_g_=g",
-    '8126063889': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&Q_DL=EMD_S8HCpCW5cI6VabX_3Ua4zpnLOCrIrX0_CGC_pXlDBDbimJuwZh4&_g_=g"}
+    '7695674373': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&RID=CGC_T0k5r0tr7YabtcY&_g_=g",
+    '8127786648': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&RID=CGC_fB93a7prL242KmD&_g_=g",
+    '7172479172': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&RID=CGC_1nFwlOgSxTcxIxz&_g_=g",
+    '8126063889': "https://iu.co1.qualtrics.com/jfe/form/SV_3Ua4zpnLOCrIrX0?Q_CHL=email&RID=CGC_pXlDBDbimJuwZh4&_g_=g"}
 
 
 def send_message():
@@ -16,7 +16,7 @@ def send_message():
         link = number_link_dict[key]
         resp = requests.post('http://textbelt.com/text', {
             'phone': str(number),
-            'message': f"Hi, please fill out the form on the link attached. Thank you {str(link)}",
+            'message': f"Hi, please fill out the form on the link attached. {str(link)}./n Please don't reply to this message. If you have any questions, email hsulon@iu.edu or call (812)7786648",
             'key': API_KEY
         })
         print(resp.json())
